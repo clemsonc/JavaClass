3d0ca6e06a43ca19ca0523bca9dad8427eeae279
Clemson Chan <clemsonc@yahoo.com>
2015-03-12 00:12:35 -0700

    Check in Chapter 2, Exercise 5
    Create a class called Dog containing two Strings: name and says. In
    main( ), create two dog objects with names “spot” (who says, “Ruff!”)
    and “scruffy” (who says, “Wurf!”). Then display their names and what
    they say.

---
+++ ch2_ex5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Dog {
+  String name;
+  String says;
+}
+
+public class ch2_ex5 {
+  public static void main(String[] args) {
+    Dog d1 = new Dog();
+    Dog d2 = new Dog();
+    d1.name = "Spot";
+    d1.says = "Ruff!";
+    d2.name = "Scruffy";
+    d2.says = "Wurf!";
+
+    System.out.println(d1.name + " says " + d1.says);
+    System.out.println(d2.name + " says " + d2.says);
+
+  }
+}
