aeb587fef9c73bc3f22d962ed1521e3c75c70f56
Clemson Chan <clemsonc@yahoo.com>
2015-03-11 23:39:47 -0700

    Check in Chapter 2, Exercise 4
    Write a program that calculates velocity using a constant distance and
    a constant time.

---
+++ ch2_ex4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ch2_ex4 {
+  public static void main(String[] args) {
+    Integer distance = 40;
+    Double time = 0.5;
+
+    System.out.println("The velocity for traveling "+distance+" miles, in "+time+" hour is "+ distance/time+" mph.");
+
+  }
+}
