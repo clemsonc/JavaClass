cc54a2813fb0691d5fd6ab3ca2cfb126424b81a2
Clemson Chan <clemsonc@yahoo.com>
2015-03-13 14:10:02 -0700

    cleaning up spacing

--- ch2_ex9.java
+++ ch2_ex9.java
@@ -1,1 +1,13 @@
-public class ch2_ex9 {  public static void main(String[] args) {    System.out.println("The smallest float number: " + Float.MIN_VALUE);    System.out.println("The largest float number: " + Float.MAX_VALUE);    System.out.println("The smallest double number: " + Double.MIN_VALUE);    System.out.println("The largest double number: " + Double.MAX_VALUE);    }}
+
+
+public class ch2_ex9 {
+  public static void main(String[] args) {
+
+    System.out.println("The smallest float number: " + Float.MIN_VALUE);
+    System.out.println("The largest float number: " + Float.MAX_VALUE);
+
+    System.out.println("The smallest double number: " + Double.MIN_VALUE);
+    System.out.println("The largest double number: " + Double.MAX_VALUE);    
+    
+  }
+}
